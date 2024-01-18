9b53d6de29ac39dbdf9af8805332ccf1f5305fc1
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-01-18 22:56:52 +0100

    Initial commit

---
+++ kafka_consumer.py
@@ -0,0 +1,36 @@
+from kafka import KafkaConsumer
+from hdfs import InsecureClient
+import uuid
+
+# Kafka Consumer
+consumer = KafkaConsumer('video_topic', bootstrap_servers='localhost:9092')
+
+# print("Waiting for messages...")
+# for message in consumer:
+#     print("Received message: ", message)
+
+# HDFS Client
+hdfs_client = InsecureClient('http://localhost:9870', user='hdfs')
+
+# HDFS target directory
+target_dir = '/student_videos'
+
+# Check and create target directory if it doesn't exist
+if not hdfs_client.content(target_dir, strict=False):
+    print(f"Creating directory: {target_dir}")
+    hdfs_client.makedirs(target_dir)
+    
+    # Set permission to 777
+    hdfs_client.chmod(target_dir, 777)
+
+def save_to_hdfs(content, filename):
+    print(f"Saving {filename} to HDFS")
+    with hdfs_client.write(f'{target_dir}/{filename}', overwrite=True) as f:
+        f.write(content)
+
+for message in consumer:
+    video_content = message.value
+
+    # Define how to set unique filenames
+    video_filename = f'video-{uuid.uuid4()}.mp4'
+    save_to_hdfs(video_content, video_filename)
---
+++ kafka_producer.py
@@ -0,0 +1,33 @@
+from kafka import KafkaProducer
+import requests
+
+# Videos links list
+videos = [
+    "https://v1.cdnpk.net/videvo_files/video/premium/getty_108/large_watermarked/istock-993951822_preview.mp4",
+    "https://v1.cdnpk.net/videvo_files/video/premium/getty_132/large_watermarked/istock-962828842_preview.mp4"
+]
+
+# Kafka Producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092', max_request_size=115343360)
+
+def download_and_send(url):
+    try:
+        print(f"Downloading video from {url}")
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            print(f"Sending video to Kafka topic")        
+            producer.send('video_topic', response.content)
+            producer.send('video_topic', 'test'.encode('utf-8'))
+
+            # Force sending of all messages
+            producer.flush()
+        else:
+            print(f"Failed to download video from {url}, status code: {response.status_code}")
+    
+    except Exception as e:
+        print(f"Error downloading/sending video: {e}")
+
+# Download and send videos
+for video in videos:
+    download_and_send(video)
