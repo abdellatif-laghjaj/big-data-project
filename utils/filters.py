08d9a7104245b89a9b72c55bea434678277e6de3
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-02-12 10:59:07 +0100

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 from scipy.signal import wiener
 
+
 def convert_to_grayscale(image):
     """Convert an image to grayscale."""
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 
 # Égalisation de l'histogramme
 def equalize_histogram(image):
@@ -23,10 +25,12 @@ def equalize_histogram(image):
 
     return image_eq
 
+
 # Appliquer un filtre gaussien avec un noyau de taille 5x5
 def apply_gaussian_blur(image, ksize=5):
     """Apply a Gaussian blur to the image."""
     return cv2.GaussianBlur(image, (ksize, ksize), 0)
+
 
 # Apply laplacian filter
 def apply_laplacian(image):
@@ -47,6 +51,7 @@ def apply_laplacian(image):
     sharpened = cv2.merge((b_lap, g_lap, r_lap))
 
     return sharpened
+
 
 # Apply Sobel filter
 def apply_sobel_filter(image):
@@ -74,15 +79,17 @@ def apply_sobel_filter(image):
 
     return sobel
 
+
 # Sharpen the image
 def sharpen_image(image):
     # Create the sharpening kernel 
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
     # Sharpen the image 
     sharpened_image = cv2.filter2D(image, -1, kernel)
 
     return sharpened_image
+
 
 # Enhance RGB image quality
 def enhance_rgb_quality(image):
@@ -98,6 +105,7 @@ def enhance_rgb_quality(image):
     enhanced_image = cv2.cvtColor(lab_image, cv2.COLOR_LAB2RGB)
 
     return enhanced_image
+
 
 # Remove blur effect from RGB image
 def remove_blur_effect(image):
@@ -135,10 +143,12 @@ def apply_median_filter(image, ksize=5):
 
     return blurred
 
-# Adjusts the brightness by adding 10 to each pixel value 
+
+# Adjusts the brightness by adding 10 to each pixel value
 def adjust_brightness(image, brightness=10, contrast=1.0):
     adjusted_image = cv2.addWeighted(image, contrast, np.zeros(image.shape, image.dtype), 0, brightness)
-    return adjusted_image  
+    return adjusted_image
+
 
 # Rotation
 def rotate_image(image, angle=90, scale=1.0):
@@ -148,10 +158,12 @@ def rotate_image(image, angle=90, scale=1.0):
     rotated_image = cv2.warpAffine(image, M, (w, h))
     return rotated_image
 
+
 # Cropping
 def crop_image(image, x, y, w, h):
-    cropped_image = image[y:y+h, x:x+w]
+    cropped_image = image[y:y + h, x:x + w]
     return cropped_image
+
 
 # Resize
 def resize_image(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -171,6 +183,7 @@ def resize_image(image, width=None, height=None, inter=cv2.INTER_AREA):
     resized_image = cv2.resize(image, dim, interpolation=inter)
     return resized_image
 
+
 # Zooming
 def zoom_image(image, zoom_factor=1.5):
     (h, w) = image.shape[:2]
@@ -178,6 +191,7 @@ def zoom_image(image, zoom_factor=1.5):
     M = cv2.getRotationMatrix2D(center, 0, zoom_factor)
     zoomed_image = cv2.warpAffine(image, M, (w, h))
     return zoomed_image
+
 
 # Edge detection
 def detect_edges(image):
@@ -194,25 +208,30 @@ def detect_edges(image):
 
     return edge
 
+
 # Dilation
 def dilate_image(image, ksize=5):
     kernel = np.ones((ksize, ksize), np.uint8)
     return cv2.dilate(image, kernel, iterations=1)
+
 
 # Erosion
 def erode_image(image, ksize=5):
     kernel = np.ones((ksize, ksize), np.uint8)
     return cv2.erode(image, kernel, iterations=1)
 
+
 # Opening
 def open_image(image, ksize=5):
     kernel = np.ones((ksize, ksize), np.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
+
 # Closing
 def close_image(image, ksize=5):
     kernel = np.ones((ksize, ksize), np.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
 
 # Bilateral filter
 def apply_bilateral_filter(image, d=3, sigmaColor=15, sigmaSpace=15):
