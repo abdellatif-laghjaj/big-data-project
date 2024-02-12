9df44e475694dd6ac08c8d164ed3f8b5328e9326
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2024-02-12 16:09:24 +0000

    test

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -3,11 +3,10 @@ from hdfs import InsecureClient
 import uuid
 
 # Kafka Consumer
-consumer = KafkaConsumer('video_topic', bootstrap_servers='localhost:9092')
-
+consumer = KafkaConsumer('video_topic', bootstrap_servers=['localhost:9092'], auto_offset_reset='smallest', group_id='newGroup')
 # print("Waiting for messages...")
 # for message in consumer:
-#     print("Received message: ", message)
+#    print("Received message: ", message)
 
 # HDFS Client
 hdfs_client = InsecureClient('http://localhost:9870', user='hdfs')
